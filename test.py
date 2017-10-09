884a6ce7e722c163f5b88109bd68c0ecb0bb923d
Jack Xie <linghuax@gmail.com>
2017-10-09 22:32:00 +1100

    add user profile

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-import datetime
-from app import models, db
-a = 257
-b = 257
-print(id(a), id(b))
-print(a is b)
+
+from functools import reduce
+strings = input('Enter your number: ')
+i = 0
+for s in strings:
+    i += int(s)
+print(i)
